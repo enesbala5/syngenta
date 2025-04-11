ec8c07479ec81729bffbc9d9716a60f56faf8793
Enes Bala <contact@enesbala.com>
2025-04-11 12:43:47 +0200

    Small updates

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,5 +3,8 @@ import { defineConfig } from 'vite';
 import { imagetools } from '@zerodevx/svelte-img/vite'
 
 export default defineConfig({
-	plugins: [sveltekit(), imagetools()]
+	plugins: [sveltekit(), imagetools()],
+	server: {
+		allowedHosts: ['localhost', '127.0.0.1', '0.0.0.0', 'syngenta.enesbala.com']
+	}
 });
